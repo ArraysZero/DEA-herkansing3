84cbeb95064cc91f4cd1cfce3d950932aa6904ec
ArraysZero <danibotland981@gmail.com>
2022-10-09 14:29:24 +0200

    start anew

---
+++ spotitube/src/main/java/nl/dani/han/App.java
@@ -0,0 +1,13 @@
+package nl.dani.han;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
